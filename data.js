c7fe408c651eae8b980a748d8c93f2a0a75ffc6c
Kat Huang <kat.m.huang@gmail.com>
2016-06-12 21:26:01 -0400

    Redo timeline styles

--- data.js
+++ data.js
@@ -52,11 +52,12 @@
         "number": 9,
         "year": "1300s",
         "event": "Renaissance Begins",
-        "long": 1
+        "long": 1,
+        "summary": "The Renaissance, named for the French word meaning “rebirth”, was an era in Western Europe lasting from the 1300s to 1500s. Thinkers during the Renaissance drew upon ancient Greek and Roman ideas to create their own works. In stark contrast with the medieval focus on religion, these humanist philosophers emphasized the value and power of the individual. Study of the humanities, subjects such as history, grammar, poetry, and rhetoric, was important. Several factors enabled culture to flourish, particularly in Italy, home to the remnants of the Roman Empire. Due to its geographical location at the intersection of trade routes, Italy also received a wealth of knowledge from the Muslim civilization to its East. In the city-state of Florence, the immensely wealthy Medici family became major financial supporters, or patrons, of the arts. Lorenzo “the Magnificent” Medici frequently invited philosophers and poets to visit his palace. Artists were often polymaths, known as “Renaissance men”. Leonardo da Vinci, for example, was not only an extraordinary painter, but also made significant contributions to the fields of anatomy, engineering, and mathematics. In general, Renaissance art more closely reflected realistic life, with the help of techniques such as perspective and lighting. The Renaissance movement spread up to Northern Europe, where writers, most notably Sir Thomas More and William Shakespeare, produced influential works in the vernacular, or everyday language. A time of changing beliefs and culture, it marked the end of the Middle Ages and the transition to modern times."
     },
     {
         "number": 10,
-        "year": "1429",
+        "year": "1431",
         "event": "Joan of Arc Leads the French",
         "summary": "During the Hundred Years’ War, 17-year-old peasant Joan of Arc told Charles VII that God had called her to save France. She led a French army to a series of victories, but was then seized by enemies, tried for witchcraft, and burned at stake. The French saw Joan as a martyr, and with renewed motivation, attacked English lands. Later, Joan of Arc was canonized as a saint by the Church."
     },
@@ -69,74 +70,88 @@
     {
         "number": 12,
         "year": "1492",
-        "event": "Spanish Reconquista / Columbus"
+        "event": "Spanish Reconquista / Columbus",
+        "summary": "The Reconquista was a movement, spurred on by the crusading spirit, to expel Muslims from the Iberian peninsula in Spain. In 1469, Ferdinand and Isabella completed the Reconquista with the downfall of the Granada. Christopher Columbus was an Italian-born explorer who sailed for Portugal and reached the islands of the Caribbean, believing he had found an alternate route to the East Indies."
     },
     {
         "number": 13,
         "year": "1501",
-        "event": "Michelangelo's David"
+        "event": "Michelangelo's David",
+        "summary": "Michelangelo was a Renaissance artist, engineer, poet, and architect whose work earned him the nickname “melancholy genius”. Only in his twenties, Michelangelo sculpted David, a statue of the Biblical figure who triumphed against Goliath, a giant. The work, reminiscent of ancient Greek style, reflects his spiritual and emotional struggles."
     },
     {
         "number": 14,
         "year": "1517",
         "event": "Luther Posts his 95 Theses",
-        "long": 1
+        "long": 1,
+        "summary": "In Wittenberg, Germany, Johann Tetzel sold indulgences to raise funds for the Cathedral of St. Peter in Rome. Indulgences granted guaranteed entry to heaven for the holders and their relatives. Martin Luther, outraged by this incident and general corruption in the Church, wrote 95 Theses against indulgences. Luther argued that indulgences were essentially scams with no biblical basis, and that eternal life and happiness could only be attained through faith. With the power of the printing press, the 95 Theses were rapidly spread across Europe. When called to recant, Luther’s beliefs became increasingly radical to the point where he was excommunicated by Pope Leo X in 1521. Luther was summoned by the Holy Roman emperor, Charles V, to the diet, or assembly of German princes, in Worms. Though he was declared an outlaw for repeatedly refusing to give up his views, Luther’s work sparked a religious revolution known as the Protestant Reformation. Many sects of Christianity formed, each differing from the Catholic Church. Lutheranism, for example, rejected some Catholic sacraments, and Calvinism was led by a council of elders, as opposed to the Pope. These changes incited conflicts through Europe and Germany. In 1555, the Peace of Augsburg was passed, letting each German prince decide whether Catholicism or Lutheranism would be the religion of their land. This was a stark difference from before, where Europe was dominated entirely by Catholicism."
     },
     {
         "number": 15,
         "year": "1522",
-        "event": "Magellan's Crew Finishes Trip"
+        "event": "Magellan's Crew Finishes Trip",
+        "summary": "Ferdinand Magellan set sail from Spain on September 20, 1519, with five ships and a full crew. Near the southern tip of South America, he traveled through what became known as the Strait of Magellan. Magellan himself was killed in the Philippines, but the remaining part of his expedition—1 ship with 18 sailors—returned to Spain, becoming the first to circumnavigate the world."
     },
     {
         "number": 16,
         "year": "1633",
-        "event": "Galileo's Trial"
+        "event": "Galileo's Trial",
+        "summary": "Galileo Galilei, a prominent scientist of the Scientific Revolution, supported the theory of heliocentrism, the idea that the planets revolved around the Sun. This contradicted Church teachings and damaged the general view that religion was flawless. After being tried by the Inquisition, he was punished with house arrest for life and forced to say geocentrism was correct."
     },
     {
         "number": 17,
         "year": "1642",
         "event": "English Civil War",
-        "long": 1
+        "long": 1,
+        "summary": "For several generations, England had been ruled by monarchs from the Tudor house, known for collaborating with Parliament. Henry VIII, though infamous for his many wives, consulted the legislative body in order to declare himself the head of the Anglican Church in the Act of Supremacy. Elizabeth I, who earned the nickname “Good Queen Bess”, had advisors who facilitated communication between her and Parliament. A series of rulers from the Stuart family succeeded the Tudors, and a multitude of political changes occurred during their reign. James I constantly requested money from Parliament not only to finance wars, but also to upkeep his lavish lifestyle and court. He opposed the Puritans, who strove to eliminate Catholic tradition from the Church of England. Charles I acted like an absolute monarch, going so far as to entirely dissolve Parliament and attempt to revive Catholic practices. In response to this, a group called the Long Parliament formed. Beginning in 1642, Charles led his Cavaliers against the Roundheads, who were led by Oliver Cromwell and who fought for Parliament. Charles’ execution in 1649 was a great shock to the rest of Europe, signalling major political changes to occur throughout the continent in the future. The House of Commons declared England a republic, called the Commonwealth, ending the English Civil War in 1651. The conflicts, however, were not yet over. The strict rules resulting from strong Puritan influence in the Commonwealth upsetted the people until the Glorious Revolution, during which the monarchy was restored."
     },
     {
         "number": 18,
         "year": "1685",
-        "event": "Louis XIV Revokes the Edict of Nantes"
+        "event": "Louis XIV Revokes the Edict of Nantes",
+        "summary": "The Edict of Nantes, issued in 1598 by Henry IV, granted religious toleration and other rights to French Protestants, known as Huguenots. Louis XIV, who took the throne in 1643, behaved like an absolute monarch. Believing that the Huguenots threatened religious unity, Louis revoked the Edict of Nantes in 1685, forcing over 100,000 French Protestants to flee. Similar to how the removal of religious minorities from Spain had harmed Spain, this loss hurt France’s economy and culture."
     },
     {
         "number": 19,
         "year": "1688",
-        "event": "The Glorious Revolution"
+        "event": "The Glorious Revolution",
+        "summary": "Stuart monarch James II was an unpopular leader, appointing Catholics to high positions and modifying laws as he wished. The English people called for James’s daughter Mary and her husband William III of Orange, both Protestants, to take over the throne. Upon their arrival, James simply fled in what became known as the Glorious Revolution, since no bloodshed was involved."
     },
     {
         "number": 20,
         "year": "1690",
-        "event": "Two Treatises of Government"
+        "event": "Two Treatises of Government",
+        "summary": "John Locke, an Enlightenment philosopher with a positive view of human nature, wrote Two Treatises of Government. He argued against absolute monarchy and insisted that the citizens should institute a system of government that ensured their natural rights. Locke’s beliefs were considered radical, especially the idea that people could overthrow their government if it failed to protect them."
     },
     {
         "number": 21,
         "year": "1748",
-        "event": "Montesquieu's \"Spirit of Laws\""
+        "event": "Montesquieu's \"Spirit of Laws\"",
+        "summary": "Baron de Montesquieu, a French thinker, learned about many societies’ political systems and described them in The Spirit of the Laws. He determined the best form of government to be divided into three branches: legislative, executive, and judicial. This separation of power, at work in the United States today, creates a system of checks and balances that prevents one part from gaining too much power."
     },
     {
         "number": 22,
         "year": "1789",
         "event": "The Storming of the Bastille",
-        "long": 1
+        "long": 1,
+        "summary": "For centuries, French society had been dictated by the ancien regime, separating the citizens into three estates. The First and Second Estates, consisting of the clergy and nobility, respectively, composed less than 3% of the population. The Third Estate, despite its magnitude, held little to no political power. The bourgeoisie, which included bankers, merchants, lawyers, doctors, and professors, were relatively well-off, but the vast majority of the population starved and struggled to survive. Meanwhile, the government was in debt and ignored the plight of its poor. Louis XIV had borrowed large sums of money to finance the Seven Years’ War and the American Revolution. Combined with the rise of prices, as well as Louis XV’s focus on pleasure and deficit spending, the government was well in debt by the time Louis XVI took the throne. French nobles, who sought to extend their power over the absolute monarch, urged Louis XVI to summon the Estates-General, a legislative body with representatives from each of the Estates. Louis XVI summoned the Estates-General for the first time in over 175 years. Each of the estates wrote down their grievances in cahiers, or notebooks. After weeks of meeting, the Estates-General had made little progress. Finally, the Third Estate delegates declared themselves as the National Assembly, representing the whole population of France. They were locked out of the halls, so they moved to a handball court, where they made the Tennis Court Oath. Some clergy and nobles sided with the National Assembly, prompting the king to reluctantly accept it. However, Louis stationed royal troops in Paris. On July 14, 1789, citizens stormed the Bastille, a prison, in search of weapons. While there were no arms to be found, the attacking of the Bastille became a symbol of the French people rising up against their oppressive government and the beginning of the French Revolution. Today, July 14 is celebrated as Bastille Day, the French Independence Day."
     },
     {
         "number": 23,
         "year": "1793",
-        "event": "The Reign of Terror"
+        "event": "The Reign of Terror",
+        "summary": "The French Revolution progressed through various stages, including the Reign of Terror, during which Maximilien Robespierre, head of the Committee of Public Safety, was in power. From September 1793 to July 1794, the guillotine became a symbol of horror, taking about 300,000 lives. When Robespierre was ironically executed by guillotine, this radical phase came to an end, giving away to a reactionary period headed by the Directory."
     },
     {
         "number": 24,
-        "year": "1804",
-        "event": "Napoleon Takes Power / Waterloo"
+        "year": "1799",
+        "event": "Napoleon Takes Power / Waterloo",
+        "summary": "Napoleon Bonaparte gained renown through a series of victories, eventually overthrowing the weak Directory and declaring himself consul for life and later Emperor of the French. Napoleon temporarily won the support of all classes and made lasting contributions such as the Napoleonic Code. He annexed the Netherlands, Belgium, and parts of Italy and Germany, but his failure to conquer Britain began his downfall. After his embarrassing defeat in Russia, Napoleon abdicated, allowing Louis XVIII to take power. Napoleon was exiled, but then returned only to be greatly defeated by the British and Prussian armies at the Battle of Waterloo in 1815."
     },
     {
         "number": 25,
         "year": "1848",
-        "event": "The Communist Manifesto"
+        "event": "The Communist Manifesto",
+        "summary": "In The Communist Manifesto, published in 1848, German philosophers Karl Marx and Friedrich Engels predicted that the working class, or proletariat, would rise against the property owners, or the bourgeoisie. In theory, the struggle would result in a classless society in which the means of production and owned by the whole community. They said that, in a larger sense, all of history was driven by economic and social inequality. They sharply criticized capitalism, which had become the dominant economic system after the Industrial Revolution, claiming that while some prospered, the vast majority suffered in poverty."
     }
 ]
